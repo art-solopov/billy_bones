f6d43cdce149312af786baf427ed4ba3cf5831e3
Artemiy Solopov <art-solopov@yandex.ru>
2016-12-24 22:05:58 +0300

    Starting the Django project and the models

---
+++ bills/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class PaymentMethod(models.Model):
+    name = models.CharField(max_length=1500)
+
+
+BILL_STATES = [
+    (1, 'new'),
+    (50, 'paid'),
+    (99, 'printed')
+]
+
+BILL_STATES_DICT = {k: name for k, name in BILL_STATES}
+
+
+class Bill(TimeStampedModel):
+    period = models.DateField(db_index=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+    state_i = models.IntegerField(choices=BILL_STATES)
+
+    paid = models.DateTimeField(null=True, blank=True)
+    printed = models.DateTimeField(null=True, blank=True)
+
+    payment_method = models.ForeignKey(
+        'PaymentMethod',
+        null=True, blank=True,
+        on_delete=models.SET_NULL
+    )
