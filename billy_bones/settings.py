a651421d7c69730359d885986330bf978bf6f99b
Artemiy Solopov <art-solopov@yandex.ru>
2017-03-19 21:50:16 +0300

    Starting basic assets

--- billy_bones/settings.py
+++ billy_bones/settings.py
@@ -122,6 +122,9 @@ TAGGIT_CASE_INSENSITIVE = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    ('bower', os.path.join(BASE_DIR, 'bower_components'))
+]
 
 # Logging
 LOGGING = {
