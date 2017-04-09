ae9bdc7e0214fa9ad9ae0ef38f41cb15540e870c
Artemiy Solopov <art-solopov@yandex.ru>
2017-04-10 00:45:37 +0300

    Preparing for production

--- billy_bones/settings.py
+++ billy_bones/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v9$xt$5z&n(*qey0hsd-9&aczi71#7y2q$q(razc&a@0(ymqfw'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    'v9$xt$5z&n(*qey0hsd-9&aczi71#7y2q$q(razc&a@0(ymqfw'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', 1))
 
 ALLOWED_HOSTS = []
 
@@ -81,7 +84,11 @@ WSGI_APPLICATION = 'billy_bones.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'billy-bones-development'
+        'NAME': os.getenv('DB_NAME', 'billy-bones-development'),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', '')
     }
 }
 
