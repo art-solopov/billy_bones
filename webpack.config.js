35ee32200d39a59e2ea77fb70ac27bbe0f20919c
Artemiy Solopov <art-solopov@yandex.ru>
2017-04-19 22:58:01 +0300

    Asset pipeline, take 2

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const env = require('process').env;
+
+const dist = path.resolve(__dirname, 'home/static/');
+
+module.exports = {
+    entry: ['./assets/js/index.js', './assets/css/index.css'],
+    output: {
+	filename: 'application.js',
+	path: dist
+    },
+    module: {
+	rules: [
+	    {
+		test: /\.(css|scss)$/,
+		use: ExtractTextPlugin.extract({
+		    fallback: 'style-loader',
+		    use: ['css-loader', 'sass-loader']
+		})
+	    },
+	    {
+		test: /\.js$/,
+		exclude: /(node_modules|bower_components)/,
+		use: {
+		    loader: 'babel-loader',
+		    options: {
+			presets: ['env']
+		    }
+		}
+	    },
+	    {
+		test: /\.(jpeg|png|gif|svg|eot|ttf|woff|woff2)$/i,
+		use: [{
+		    loader: 'file-loader',
+		    options: {
+			dist,
+			name: env.NODE_ENV === 'production' ? '[name]-[hash].[ext]' : '[name].[ext]',
+			publicPath: env.NODE_ENV === 'production' ? '/static/' : 'http://localhost:12800/'
+		    }
+		}]
+
+	    }
+	]
+    },
+    plugins: [
+	new ExtractTextPlugin('application.css')
+    ],
+    devServer: {
+	contentBase: dist,
+	compress: true,
+	port: 12800
+    }
+};
