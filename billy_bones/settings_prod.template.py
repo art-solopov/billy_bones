21483b0c84f76a556e41c312c3990ebf533b2ded
Artemiy Solopov <art-solopov@yandex.ru>
2017-06-04 15:24:08 +0300

    Deploy vars and Ansible templates

--- billy_bones/settings_prod.template.py
+++ billy_bones/settings_prod.template.py
@@ -1,24 +1,22 @@
 import os
 from .settings import *
 
-# Fill the blanks!
-
-SECRET_KEY = '{}'
+SECRET_KEY = '{{ django_secret_key }}'
 DEBUG = False
-ALLOWED_HOSTS = ['{}']
+ALLOWED_HOSTS = ['{{ django_host }}']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '{}',
-        'HOST': '{}',
-        'USER': '{}',
-        'PASSWORD': '{}'
+        'NAME': '{{ django_db.name }}',
+        'HOST': '{{ django_db.host }}',
+        'USER': '{{ django_db.user }}',
+        'PASSWORD': '{{ django_db.password }}'
     }
 }
 
 MANIFEST_PATH = os.path.join(BASE_DIR, 'home', 'static', 'manifest.json')
-STATIC_ROOT = os.path.join(BASE_DIR, '__static')
+STATIC_ROOT = '{{ django_staticroot }}'
 
 for lgr in LOGGING['loggers'].values():
     lgr['handlers'] = ['file']
