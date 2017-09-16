c241d81e97b73ee3b6ebb9bcb2fbc2d0e0726d13
Artemiy Solopov <art-solopov@yandex.ru>
2017-09-17 00:25:49 +0300

    Continuing the Note views.

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import (ListNotes)
+from .views import (ListNotes, CreateNote)
 
 app_name = 'notes'
 
 urlpatterns = [
-    url(r'^/?$', ListNotes.as_view(), name='index')
+    url(r'^$', ListNotes.as_view(), name='index'),
+    url(r'^new$', CreateNote.as_view(), name='new'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
-# from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic.edit import (CreateView, UpdateView)
 
 from .models import Note
 
@@ -8,4 +9,14 @@ class ListNotes(ListView):
     template_name = 'notes/list.html'
     queryset = Note.objects.order_by('note_type', 'created')
 
-# Create your views here.
+
+class CUMixin:
+    model = Note
+    fields = ['note_type', 'title', 'text']
+    success_url = reverse_lazy('notes:index')
+
+class CreateNote(CUMixin, CreateView):
+    template_name = 'notes/create.html'
+
+class UpdateNote(CUMixin, UpdateView):
+    template_name = 'notes/update.html'
